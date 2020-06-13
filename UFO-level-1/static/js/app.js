5dd8347c25fce2d868f661eccd8b59135b04b34a
niccc <nfernandocolon@gmail.com>
2020-06-13 11:28:26 -0400

    part 1 working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -20,12 +20,13 @@ addRows(tableData);
 var filterButt = d3.select("#filter-btn");
 var inputDate = d3.select("#datetime");
 
-filterButt.on('click', runEnter);
-
 var runEnter = () => {
   d3.event.preventDefault();
+  d3.selectAll("tr").remove();
   var filterDate = inputDate.property("value");
   var filterSightings = data.filter(data => data.datetime == filterDate);
-
   addRows(filterSightings);
+  console.log(filterDate);
 }
+
+filterButt.on('click', runEnter);
